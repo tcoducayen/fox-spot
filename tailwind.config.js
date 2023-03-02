e423ac91331f3c03a183125f6852db3e7272af8d
Nova <tcoducayen@gmail.com>
2023-03-02 15:04:42 -0800

    Updating a more personalized index with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,tsx}',
+    './public/**/*.{js,ts,tsx}',
+    './styles/**/*.{js,ts,tsx}'
+  ],
+  theme: {},
+  plugins: [],
+}
